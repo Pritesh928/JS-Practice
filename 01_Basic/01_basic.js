4524ef3229dbff34256ec3330a893d7ba3de4278
Pritesh <pptsingh123@gmail.com>
2025-09-27 22:15:50 +0530

    D1

---
+++ 01_Basic/01_basic.js
@@ -0,0 +1,6 @@
+const accID = 1234;
+let accmail = "Helelor@gmail.com"
+var accname = "Pritesh"
+acccity = "Mumbai"
+let abc;
+console.table([accID,accmail,accname,acccity,abc])
---
+++ 01_Basic/02_basic.js
@@ -0,0 +1,6 @@
+"use strict" //it declared that we are using all newer js version concepts
+//alert(3 + 3) //we are using nodejs not browser 
+
+console.log(3 + 3)
+console.log("Pritesh")
+
