bbb526e42b624b65ae8914158e6e616ea9ecb5ea
Pritesh <pptsingh123@gmail.com>
2025-09-28 14:32:12 +0530

    D2

---
+++ 01_Basic/04_basic.js
@@ -0,0 +1,18 @@
+let value = 10
+let negvalue = -value
+console.log(negvalue);
+//
+console.log(3 + 1 + "2");
+//
+console.log("3" + 1 + 2);
+//
+console.log(+true);
+//
+gamecounter = 12
+++gamecounter
+console.log(gamecounter);
+console.log(gamecounter);
+//
+
+
+
